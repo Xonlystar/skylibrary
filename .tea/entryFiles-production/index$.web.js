9bc332cd33cb953d863d29271b7e54d70507510b
徐辉波 <991923257@qq.com>
2019-05-22 21:09:47 +0800

    项目初始化

---
+++ .tea/entryFiles-production/index$.web.js
@@ -0,0 +1,6 @@
+require('@alipay/appx-compiler/lib/sjsEnvInit');
+require('./config$');
+require('../../pages/blog/index');
+require('../../pages/book/index');
+require('../../pages/store/index');
+require('../../pages/user/index');
---
+++ .tea/entryFiles-production/index$.worker.js
@@ -0,0 +1,27 @@
+if(!self.__appxInited) {
+self.__appxInited = 1;
+
+
+require('./config$');
+require('./importScripts$');
+
+var AFAppX = self.AFAppX;
+self.getCurrentPages = AFAppX.getCurrentPages;
+self.getApp = AFAppX.getApp;
+self.Page = AFAppX.Page;
+self.App = AFAppX.App;
+self.my = AFAppX.bridge || AFAppX.abridge;
+self.abridge = self.my;
+self.Component = AFAppX.WorkerComponent || function(){};
+self.$global = AFAppX.$global;
+
+
+function success() {
+require('../..//app');
+require('../../pages/blog/index');
+require('../../pages/book/index');
+require('../../pages/store/index');
+require('../../pages/user/index');
+}
+self.bootstrapApp ? self.bootstrapApp({ success }) : success();
+}
